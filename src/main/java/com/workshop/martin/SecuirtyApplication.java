5b1337ef73ef6dbedeed0ee4866761783b4696be
Martin Ondrejka <martin.ondrejka@gmail.com>
2023-09-20 09:52:24 +0200

    SecurityFilterChain v1.00

---
+++ src/main/java/com/workshop/martin/SecuirtyApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.martin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuirtyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuirtyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/workshop/martin/config/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.workshop.martin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http
+				.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+				.build();
+	}
+}
