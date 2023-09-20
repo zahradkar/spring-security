d64bccd824bac5383929fb0f460a8414ba5b1b84
Martin Ondrejka <martin.ondrejka@gmail.com>
2023-09-20 10:49:54 +0200

    added Role

--- src/main/java/com/workshop/martin/controllers/models/Role.java
+++ src/main/java/com/workshop/martin/controllers/models/Role.java
@@ -8,10 +8,29 @@ import org.springframework.security.core.GrantedAuthority;
 public class Role implements GrantedAuthority {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long roleId;
+	@Column(name = "role_id")
+	private byte roleId;
+
+	private String authority;
+
+	public Role() {
+		super();
+	}
+
+	public Role(String authority) {
+		this.authority = authority;
+	}
+
+	public byte getRoleId() {
+		return roleId;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
 
 	@Override
 	public String getAuthority() {
-		return null;
+		return this.authority;
 	}
 }
